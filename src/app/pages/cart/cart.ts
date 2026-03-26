525d17d4e769b42080a08d31cac4653bc673b2b5
DioYato <pie1@live.it>
2026-03-26 12:24:01 +0100

    carrello

--- src/app/pages/cart/cart.ts
+++ src/app/pages/cart/cart.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { CartService } from '../../data/cart.service';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  standalone: true,
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
-export class Cart {
+export class CartComponent {
 
+  private readonly cart = inject(CartService);
+
+  readonly items = computed(() => this.cart.all());
+  readonly total = computed(() => this.cart.total());
 }
