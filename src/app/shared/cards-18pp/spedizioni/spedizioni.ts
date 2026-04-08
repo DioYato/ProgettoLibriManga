2694c985fb0704fa96c7995ba8e79c982681ab2c
Angelo <argentieroangelo4@gmail.com>
2026-04-08 14:40:50 +0200

    e fixa sto computer

---
+++ src/app/shared/cards-18pp/spedizioni/spedizioni.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spedizioni',
+  imports: [],
+  templateUrl: './spedizioni.html',
+  styleUrl: './spedizioni.css',
+})
+export class Spedizioni {
+
+}
---
+++ src/app/shared/promotion-section/promotion-section.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promotion-section',
+  imports: [],
+  templateUrl: './promotion-section.html',
+  styleUrl: './promotion-section.css',
+})
+export class PromotionSection {
+
+ 
+
+}
