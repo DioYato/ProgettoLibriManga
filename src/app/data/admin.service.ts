49e372c162d59020f15d3ef72b05fc9482080dc0
DioYato <pie1@live.it>
2026-04-09 16:09:48 +0200

    css home

--- src/app/data/admin.service.ts
+++ src/app/data/admin.service.ts
@@ -73,10 +73,6 @@ export class AdminService {
     );
   }
 
-  /**
-   * MODIFICATO: Ora accetta un oggetto (JSON) perché il backend 
-   * non supporta multipart/form-data ma vuole application/json.
-   */
   addProduct(libroData: any): Observable<LibriResponse> {
     // Inviando libroData come oggetto, HttpClient imposta automaticamente Content-Type: application/json
     return this.http.post<LibriResponse>(`${this.api}/libri/create`, libroData);
