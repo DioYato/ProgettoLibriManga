21325ff3eeb9ecfdc2c1bb3a416ef6715a45d1b8
DioYato <pie1@live.it>
2026-04-17 11:28:09 +0200

    cavolate

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./shared/navbar/navbar";
 import { Footer } from './shared/footer/footer';
+import { ChatWidget } from './chat-widget/chat-widget';
+
+
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, ChatWidget],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
